3ec4f5f102cf29e6cf5473594b6ac385162cebe3
Rob Cannon <rpcann@gmail.com>
2023-05-29 01:45:13 -0400

    Some basic settings and countdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,13 @@
 		(total, { start, stop }) => total + Math.round(stop.getTime() - start.getTime()),
 		0
 	);
-	$: totalTime = exisitingTime + Math.round(currentTime);
+	$: totalTime = projectHasTimeLimit
+		? parseInt(projectTimeLimitDuration) - (exisitingTime + Math.round(currentTime))
+		: exisitingTime + Math.round(currentTime);
+
+	let projectName = 'New Project';
+	let projectHasTimeLimit = false;
+	let projectTimeLimitDuration = '0';
 </script>
 
 <div class="flex flex-col justify-center gap-8">
@@ -22,12 +28,37 @@
 	/>
 	<div class="text-center font-mono text-6xl">{getTimeDisplay(totalTime)}</div>
 	<div class="flex flex-col gap-2">
+		<h2>Project Settings</h2>
+		<label for="project_name">
+			Name
+			<input id="project_name" bind:value={projectName} />
+		</label>
+		<h3>Time Limit</h3>
+		<div class="flex flex-col gap-2 pl-4">
+			<label for="has_time_limit">
+				Enable
+				<input id="has_time_limit" type="checkbox" bind:checked={projectHasTimeLimit} />
+			</label>
+			<label for="time_limit">
+				Duration
+				<input
+					id="time_limit"
+					disabled={!projectHasTimeLimit}
+					bind:value={projectTimeLimitDuration}
+				/>
+			</label>
+		</div>
+	</div>
+	<div class="flex flex-col gap-2">
+		<h2>Past Sessions</h2>
 		{#each list as item}
 			<div class="flex flex-row gap-4">
 				<div>{getTimeDisplay(Math.round(item.stop.getTime() - item.start.getTime()))}</div>
 				<div>{item.start.toLocaleString()}</div>
 				<div>{item.stop.toLocaleString()}</div>
 			</div>
+		{:else}
+			<div>No sessions logged so far</div>
 		{/each}
 	</div>
 </div>
