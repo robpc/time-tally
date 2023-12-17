e0ef9874106d8c333c7baeba462101f8f9ea0b83
Rob Cannon <rpcann@gmail.com>
2023-12-16 23:26:24 -0500

    Fix back link

--- src/routes/project/[id]/+page.svelte
+++ src/routes/project/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import { myStore } from '$lib/store';
 
@@ -17,5 +18,5 @@
 	<Project bind:project={$application.projects[idx]} />
 {:else if !$application.loading}
 	<h1 class="text-2xl mb-4">Project Not found</h1>
-	<div><a href="/" class="hover:underline text-blue-400">Return to project list</a></div>
+	<div><a href={`${base}/`} class="hover:underline text-blue-400">Return to project list</a></div>
 {/if}
